64a08cac87e88e1e0d219a9fd9f691f98caada7f
Olivier Spinelli <olivier.spinelli@invenietis.com>
2018-04-20 11:48:57 +0200

    Meeting with Guests. TODO: compute the ArrivalFlights & DepartureFlights.

--- Algo-Reco/Algo.Optim/Meeting.cs
+++ Algo-Reco/Algo.Optim/Meeting.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,19 +7,60 @@ namespace Algo.Optim
 {
     public class Guest
     {
-        public string Name { get; set; }
+        public Guest( Meeting m,
+                      string name,
+                      Airport location )
+        {
+            Name = name;
+            Location = location;
 
-        public Airport Location { get; set; }
+        }
+
+        public string Name { get; }
+
+        public Airport Location { get; }
+
+        /// <summary>
+        /// No stop is flights required.
+        /// (Unused, this is just a sample of constraint.)
+        /// </summary>
+        public bool NoStop { get; set; }
+
+        public IReadOnlyList<SimpleFlight> ArrivalFlights { get; }
+
+        public IReadOnlyList<SimpleFlight> DepartureFlights { get; }
     }
 
     public class Meeting
     {
-        public Meeting()
+        public Meeting( FlightDatabase db )
         {
+            FlightDatabase = db;
             Location = Airport.FindByCode( "LHR" );
+            MaxBusTimeOnArrival = new DateTime( 2010, 7, 27, 17, 0, 0 );
+            MinBusTimeOnDeparture = new DateTime( 2010, 8, 3, 15, 0, 0 );
+            Guests = new[]
+            {
+                new Guest( this, "Kaï", Airport.FindByCode( "BER" ) ),
+                new Guest( this, "Erwan", Airport.FindByCode( "CDG" ) ),
+                new Guest( this, "Robert", Airport.FindByCode( "MRS" ) ),
+                new Guest( this, "Paul", Airport.FindByCode( "LYS" ) ),
+                new Guest( this, "James", Airport.FindByCode( "MAN" ) ),
+                new Guest( this, "Pedro", Airport.FindByCode( "BIO" ) ),
+                new Guest( this, "John", Airport.FindByCode( "JFK" ) ),
+                new Guest( this, "Abdel", Airport.FindByCode( "TUN" ) ),
+                new Guest( this, "Isabella", Airport.FindByCode( "MXP" ) )
+            };
         }
 
-        public Airport Location { get; private set; }
+        public FlightDatabase FlightDatabase { get; }
 
+        public Airport Location { get; }
+
+        public DateTime MaxBusTimeOnArrival { get; }
+
+        public DateTime MinBusTimeOnDeparture { get; }
+
+        public IReadOnlyList<Guest> Guests { get; }
     }
 }
